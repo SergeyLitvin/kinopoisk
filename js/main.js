9097fb84ec1b2fe4a1f54a7860f9f8a857cee5b5
Front-end Dev <front.dev@cryptobitup.net>
2019-07-01 23:51:22 +0300

    Render base structure listMoies in html

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
 
 const apiKey = '1b881d5b372353011a0eae96576a19ca';
 const searchForm = document.getElementById('search-form');
+const movies = document.getElementById('movies');
 
 function apiSearch(event) {
   event.preventDefault();
   const searchText = document.querySelector('.form-control').value;
 
   const requestUrl = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=ru-RU&query=${searchText}&include_adult=false`;
- 
-  getServerData(requestUrl).then(function(data) {
+  
+  movies.innerHTML = 'Загрузка результатов поиска...';
+
+  const serverData = getServerData(requestUrl).then(function(data) {
     const searchingResultsData = JSON.parse(data).results;
+
+    let listMovies = '';
+    searchingResultsData.forEach((el) => {
+      let nameEl = el.name || el.title;
+      let imgUrl = 'https://image.tmdb.org/t/p/w500/'
+      listMovies += `
+      <article class="col-xs-12 col-md-6">
+        <h1>${nameEl}</h1>
+        <img src="${imgUrl + el.poster_path}">
+      </article>
+    `;
+
+      movies.innerHTML = listMovies;
+    });
+    
     console.log(searchingResultsData);
+    // return searchingResultsData;
+
   }, function(error) {console.error()});
   // console.log(requestUrl);
-  
+  return serverData;
 }
 
 
