c566fc0fadd60e976d43341f2d36b0564d147e6a
Front-end Dev <front.dev@cryptobitup.net>
2019-07-09 00:36:22 +0300

    Add descript fot card films

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ function renderData(data) {
         <h1>${nameEl}</h1>
         <img src="${imgUrl + el.poster_path}">
         <div class="descript">
-          <span class="year">Год: ${el.release_date.slice(0, 4)}</span> 
+          <span class="year">Год: ${el.release_date}</span> 
           <span class="rating">Рейтинг: ${el.vote_average}</span>  
         </div>
       </article>
